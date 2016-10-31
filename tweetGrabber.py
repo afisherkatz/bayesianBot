271e735c3c57af892ebda310a28420fe063357c8
Aaron Fisher-Katz <afisherkatz@gmail.com>
2016-10-31 20:17:47 +0000

    tweet grabbing capabilities added

--- tweetGrabber.py
+++ tweetGrabber.py
@@ -1,3 +1,8 @@
+import sys
 import twitter
+from twitterCredentials import api
 
+results = api.GetSearch(raw_query="q=%23" + sys.argv[1] + "&count=1000")
+results = [res.text for res in results]
+print(str(results).encode('utf-8'))
 
